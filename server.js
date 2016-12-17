afdf92ae68f421adf561aebd50da4395b9f4ab13
Luca Peduto <luca@eloquentbit>
2016-12-17 10:34:21 +0000

    Add parse date endpoint

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const moment = require('moment');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/:date', function(req, res) {
+    
+    var parseDate = moment(req.params.date, 'MMMM D YYYY');
+    
+    if (parseDate.isValid()) {
+        res.json({
+            unix: parseDate.format('X'),
+            natural: parseDate.format('MMMM D, YYYY')
+        });
+    } else {
+        res.json({unix: 'null', natural: 'null'});
+    }
 });
 
 app.listen('8080', function() {
-    console.log('Example app listening on port 8080');
+    console.log('Timestamp app listening on port 8080');
 });
